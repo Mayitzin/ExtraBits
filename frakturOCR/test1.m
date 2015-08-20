a39dc8c9fd480db8fe58071658d65eab5bc30fd2
Mario Garcia <mayitzin@gmail.com>
2015-08-20 14:28:43 +0200

    Added Binarization and Histograms

--- frakturOCR/test1.m
+++ frakturOCR/test1.m
@@ -11,31 +11,51 @@
 % @author: Mario Garcia
 % www.mayitzin.com
 
-%clear all
-%
-%disp('Starting test')
-%
-%I = imread('page001.png');
-%I = rgb2gray(I);
-[m, n] = size(I);
-f1 = fspecial("unsharp", 0.1);
-f2 = fspecial("unsharp", 0.9);
+% clear all
+% 
+% disp('Starting test')
+% 
+% I = imread('page001.png');
+% I = rgb2gray(I);
+% [m, n] = size(I);
+% 
+% % Sharpening Filters
+% f1 = fspecial('unsharp', 0.1);
+% f2 = fspecial('unsharp', 0.9);
+% 
+% % Sharped Images
+% J1 = imfilter(I,f1);
+% J2 = imfilter(I,f2);
+% 
+% % Binarized Images
+% t = 220;
+% Ibw1 = im2bw(J1,t/256);
+% Ibw2 = im2bw(J2,t/256);
 
-J1 = imfilter(I,f1);
-J2 = imfilter(I,f2);
-t = 200;
-Ibw1 = im2bw(J1,t/256);
-Ibw2 = im2bw(J2,t/256);
+% Dilate image vertically
+se2 = strel('square',5);
+Idi2 = ~imdilate(~Ibw2,se2);
 
+% Centroids
+Ics2 = regionprops(~Idi2,'centroid');
+cs2 = cat(1, Ics2.Centroid);
 
-subplot(1,3,1)
-imshow(I(800:1600,1:500));
-subplot(1,3,2)
-imshow(Ibw1(800:1600,1:500));
-subplot(1,3,3)
-imshow(Ibw2(800:1600,1:500));
+% Histograms
+x = sum(Ibw2,2);
+% figure()
+% hist(cs2(:,2),n);
 
-%subplot(2,3,[1 4])
-%imshow(I)
-%subplot(2,3,[2 3])
-%hist(I,256)
+% Plotting Images
+figure()
+subplot(1,4,1)
+imshow(Ibw2);
+subplot(1,4,2)
+imshow(Idi2); hold on
+plot(cs2(:,1),cs2(:,2), 'r*'); hold off
+subplot(1,4,3) % Histogram of Pixels
+hist(x, m);
+% hist(gca, x, m);
+% set(gca,'view',[90 90])
+subplot(1,4,4) % Histogram of Points
+hist(gca, cs2(:,2), m);
+set(gca,'view',[90 90])

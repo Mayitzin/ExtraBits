72b2312ccdfd1b1599eb54022abdc12349e16674
Mario Garcia <mayitzin@gmail.com>
2015-08-23 23:49:21 +0200

    Added mixed histograms

--- frakturOCR/test1.m
+++ frakturOCR/test1.m
@@ -11,26 +11,26 @@
 % @author: Mario Garcia
 % www.mayitzin.com
 
-% clear all
-% 
-% disp('Starting test')
-% 
-% I = imread('page001.png');
-% I = rgb2gray(I);
-% [m, n] = size(I);
-% 
-% % Sharpening Filters
-% f1 = fspecial('unsharp', 0.1);
-% f2 = fspecial('unsharp', 0.9);
-% 
-% % Sharped Images
-% J1 = imfilter(I,f1);
-% J2 = imfilter(I,f2);
-% 
-% % Binarized Images
-% t = 220;
-% Ibw1 = im2bw(J1,t/256);
-% Ibw2 = im2bw(J2,t/256);
+clear all
+
+disp('Starting test')
+
+I = imread('page001.png');
+I = rgb2gray(I);
+[m, n] = size(I);
+
+% Sharpening Filters
+f1 = fspecial('unsharp', 0.1);
+f2 = fspecial('unsharp', 0.9);
+
+% Sharped Images
+J1 = imfilter(I,f1);
+J2 = imfilter(I,f2);
+
+% Binarized Images
+t = 220;
+Ibw1 = im2bw(J1,t/256);
+Ibw2 = im2bw(J2,t/256);
 
 % Dilate image vertically
 se2 = strel('square',5);
@@ -42,22 +42,26 @@ cs2 = cat(1, Ics2.Centroid);
 
 % Pseudo-Histograms
 x = sum(~Ibw2,2);
-y = zeros(m);
+y = zeros(m,1);
 for i = 1:m
-    y(i) = sum(find(round(cs2(:,2))==i));
+    y(i) = sum(round(cs2(:,2))==i);
 end
+z = x.*y;
 
 % Plotting Images
 figure()
+% subplot(1,4,1)
+%     imshow(I);
 subplot(1,4,1)
-    imshow(I);
-subplot(1,4,2)
-    imshow(Idi2); hold on
+    imshow(I); hold on
     plot(cs2(:,1),cs2(:,2), 'r*'); hold off
-subplot(1,4,3) % Histogram of Pixels
+subplot(1,4,2) % Histogram of Pixels
     plot(gca, 1:m,x, 'r-')
     set(gca,'view',[90 90])
-subplot(1,4,4) % Histogram of Points
+subplot(1,4,3) % Histogram of Points
     plot(gca, 1:m,y, 'r-')
+    set(gca,'view',[90 90])
+subplot(1,4,4)
+    plot(gca, 1:m,z, 'r-')
     set(gca,'view',[90 90])
 

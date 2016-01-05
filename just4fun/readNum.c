04115ae64ca6737607445b0c47265bd67b25a450
Mario Garcia <mayitzin@gmail.com>
2016-01-05 18:09:14 +0100

    Gets number of digits with log10

--- just4fun/readNum.c
+++ just4fun/readNum.c
@@ -8,11 +8,20 @@
  */
 
 #include <stdio.h>
+#include <math.h>
 
 int main(int argc, char *argv[]){
     int num = atoi(argv[1]);
+    int nDigits;
+
+    if (num==0){
+        nDigits = 1;
+    } else {
+        nDigits = floor(log10(abs(num))) + 1;
+    }
 
     printf("%d\n", num);
+    printf("%d\n", nDigits);
 
     return (0);
 }

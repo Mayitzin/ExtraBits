cfd266620742d176b80f2bc178052738cc64c1ae
Mario Garcia <mayitzin@gmail.com>
2016-01-05 16:35:14 +0100

    Added number phonetic reader

---
+++ just4fun/readNum.c
@@ -0,0 +1,18 @@
+/*
+ * Read Numbers
+ *
+ * @brief A program that writes down the pronuntiation of each number
+ *
+ * @author Mario Garcia
+ * www.mayitzin.com
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    int num = atoi(argv[1]);
+
+    printf("%d\n", num);
+
+    return (0);
+}

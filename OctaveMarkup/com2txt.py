2af5aeab1a99355f82435a8eb79d210c8fef39fa
Mario Garcia <mayitzin@gmail.com>
2016-02-01 23:57:31 +0100

    Added NLTK

--- OctaveMarkup/com2txt.py
+++ OctaveMarkup/com2txt.py
@@ -3,19 +3,33 @@ Parser of M-files to get the info from comments.
 
 History:
     24.05.2015. First implementation.
+    01.02.2016. Using NLTK
 
 @author: Mario Garcia
 www.mayitzin.com
 """
 
-with open('rotation.m', 'r') as inputFile:
+import nltk
+
+fileName = 'rotation.m'
+
+with open(fileName, 'r') as inputFile:
     text = inputFile.readlines()
 
 m = len(text)
+lines = []
 
 for i in range(m):
 	if text[i][0] == "%":
+		lines.append(text[i])
 		ttype = "block"
-		print i, ":", len(text[i]), ":", text[i][:-1]
+		print i, nltk.word_tokenize(text[i])
+		# print i, ":", len(text[i]), ":", text[i][:-1]
 
-print "Finished"
+print nltk.word_tokenize(lines[0])
+print fileName[:-2].upper()
+
+if nltk.word_tokenize(lines[1])==fileName[:-2].upper():
+	print "the title of the function is", fileName[:-2]
+else:
+	print "The name of the file does not correspond to the title"

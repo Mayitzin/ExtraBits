bd8902a4f094eb531c7b369b5d5c52c978bc1be7
Mario Garcia <mayitzin@gmail.com>
2016-03-04 05:01:08 +0100

    Added buildHTML function

--- OctaveMarkup/buildHTML.py
+++ OctaveMarkup/buildHTML.py
@@ -9,17 +9,23 @@ History:
 www.mayitzin.com
 """
 
+def buildHTML(body,head):
+    doctype = "<!DOCTYPE html>"
+    html_open = "<html>"
+    body = "<body>Hi!</body>"
+    html_close = "</html>"
+    return doctype+html_open+head+body+html_close
+
 fileName = 'Output.html'
 
-text = """<!DOCTYPE html>
-<html>
+header = """
 <head>
-<title>Test</title>
+    <title>Test</title>
 </head>
-<body>
-Hi!
-</body>
-</html>"""
+"""
+body = "<body>Hi!</body>"
+
+text = buildHTML(body,header)
 
 with open(fileName, 'w') as outputFile:
     outputFile.write(text)

53419d22047ce4a6cbb24690964feef6fed293ae
Mario Garcia <mayitzin@gmail.com>
2016-03-07 20:33:54 +0100

    Added function splitting terms

--- just4fun/simplify.py
+++ just4fun/simplify.py
@@ -14,3 +14,15 @@ def isFile(inputFile):
     validFiles = [".csv", ".txt"]
     if inputFile[-4:] in validFiles: return True
     else: return False
+
+def getParticles(dummy):
+    n = len(dummy)
+    positions, particles = [], {}
+    for i in range(n):
+        if dummy[i]=="(":
+            positions.append(i)
+        if dummy[i]==")":
+            part = dummy[positions.pop():i+1]
+            if part not in particles.keys(): particles[part] = 1
+            else: particles[part] += 1
+    return particles, positions

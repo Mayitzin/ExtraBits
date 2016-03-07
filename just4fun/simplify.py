b3c3f868380bb843712c163fa51793a9df9fb433
Mario Garcia <mayitzin@gmail.com>
2016-03-07 20:35:49 +0100

    Added function that replaces constant values

--- just4fun/simplify.py
+++ just4fun/simplify.py
@@ -15,6 +15,7 @@ def isFile(inputFile):
     if inputFile[-4:] in validFiles: return True
     else: return False
 
+
 def getParticles(dummy):
     n = len(dummy)
     positions, particles = [], {}
@@ -26,3 +27,11 @@ def getParticles(dummy):
             if part not in particles.keys(): particles[part] = 1
             else: particles[part] += 1
     return particles, positions
+
+
+def replaceConst(dummy):
+    dummy = dummy.replace("dt**","dt")
+    dummy = dummy.replace("dt^","dt")
+    return dummy
+
+

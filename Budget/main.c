8cd5b3a26195697d9a86251e1e8a9aab97c7dcb9
Mario Garcia <mayitzin@gmail.com>
2016-05-06 19:11:05 +0200

    Add individual set of string using strcpy

--- Budget/main.c
+++ Budget/main.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <string.h>
 
 #define LINE_LENGTH 500
 #define NAME_LENGTH 100
@@ -30,15 +31,25 @@ void die(const char *message) {
     exit(1);
 }
 
+// void setInfo(char *name, int id, char *name, char *email){}
+
 
 // ------------------------------- MAIN ROUTINE -------------------------------
 
 int main(int argc, char *argv[]){
     printf("First Test\n");
 
+    // First Transaction
     struct transaction t1 = {.id = 123, .name = "Mario"};
-
     transaction_print(&t1);
+
+    // Second Transaction
+    struct transaction *t2 = malloc(sizeof(struct transaction));
+    if(!t2) die("Memory error");
+    t2->id = 234;
+    strcpy(t2->name, "Eduardo");
+    strcpy(t2->email, "eddy@gmail.com");
+    transaction_print(t2);
 
     return 0;
 }

48926dd18898f37a9f824c4851dbcdd48536e1e7
Mario Garcia <mayitzin@gmail.com>
2016-05-06 11:51:17 +0200

    Add Bduget Controller Project

---
+++ Budget/main.c
@@ -0,0 +1,13 @@
+/* =================
+ * Budget Controller
+ * =================
+ *
+ * @author: Mario Garcia
+**/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+	printf("First Test\n");
+	return 0;
+}

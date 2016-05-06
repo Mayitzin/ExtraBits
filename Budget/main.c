d32dc18ce7e9878ab94d82453f7198083d45791a
Mario Garcia <mayitzin@gmail.com>
2016-05-06 13:10:28 +0200

    Add print data and die function

--- Budget/main.c
+++ Budget/main.c
@@ -6,8 +6,39 @@
 **/
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#define LINE_LENGTH 500
+#define NAME_LENGTH 100
+#define MAIL_LENGTH 50
+
+struct transaction {
+    int id;
+    char name[NAME_LENGTH];
+    char email[MAIL_LENGTH];
+};
+
+
+void transaction_print(struct transaction *tran) {
+    printf("%d %s %s\n",  tran->id,  tran->name,  tran->email);
+}
+
+void die(const char *message) {
+    if(errno) { perror(message); }
+    else { printf("ERROR: %s\n", message); }
+    exit(1);
+}
+
+
+// ------------------------------- MAIN ROUTINE -------------------------------
 
 int main(int argc, char *argv[]){
-	printf("First Test\n");
-	return 0;
+    printf("First Test\n");
+
+    struct transaction t1 = {.id = 123, .name = "Mario"};
+
+    transaction_print(&t1);
+
+    return 0;
 }

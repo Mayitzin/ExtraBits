562ddf017e508dd64a4190e89a8c8acc8150da03
Mario Garcia <mayitzin@gmail.com>
2016-05-08 01:44:53 +0200

    Add getTrans function

--- Budget/main.c
+++ Budget/main.c
@@ -31,7 +31,18 @@ void die(const char *message) {
     exit(1);
 }
 
-// void setInfo(char *name, int id, char *name, char *email){}
+void setInfo(struct transaction *tran, int id, char *name, char *email) {
+    tran->id = id;
+    strcpy(tran->name, name);
+    strcpy(tran->email, email);
+}
+
+struct transaction *getTrans(int id, char *name, char *email) {
+    struct transaction *t4 = malloc(sizeof(struct transaction));
+    if(!t4) die("Memory error");
+    setInfo(t4, id, name, email);
+    return t4;
+}
 
 
 // ------------------------------- MAIN ROUTINE -------------------------------
@@ -50,6 +61,16 @@ int main(int argc, char *argv[]){
     strcpy(t2->name, "Eduardo");
     strcpy(t2->email, "eddy@gmail.com");
     transaction_print(t2);
+
+    // Third Transaction
+    struct transaction *t3 = malloc(sizeof(struct transaction));
+    if(!t3) die("Memory error");
+    setInfo(t3, 345, "Renske", "renske@mail.com");
+    transaction_print(t3);
+
+    // Fourth Transaction
+    struct transaction *t4 = getTrans(456, "Johanna", "johanna@mail.com");
+    transaction_print(t4);
 
     return 0;
 }

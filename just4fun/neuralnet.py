d0b010a4c95715a1e8824af14a0585ba76ccfe28
Mayitzin <mayitzin@gmail.com>
2016-06-22 18:02:56 +0200

    Add weighthed sums function

--- just4fun/neuralnet.py
+++ just4fun/neuralnet.py
@@ -10,19 +10,49 @@ http://www.existor.com/en/ml-neural-networks.html
 import numpy as np
 import matplotlib.pyplot as plt
 
+def w_sums(x, w):
+    if len(np.shape(w))<2:
+        z = np.array([[0.0]])
+        w = np.array(w).reshape((len(w),1))
+    else:
+        z = np.zeros((np.shape(w)[0],1))
+    print np.shape(z)
+    print np.shape(w)
+    for i in range(np.shape(w)[0]):
+        for j in range(np.shape(z)[1]):
+            print "w[%d,%d]=%f"%(i,j,w[i,j])
+            # z[i] += x[i]*w[i,j]
+    return z
+
 def sigmoid(x):
     return 1./(1+np.exp(-x))
 
 def nn_tanh(x):
     return np.tanh(x)
 
-x = 0.0
-print "sigmoid(%.5f) = %.5f" % (x,sigmoid(x))
-print "tanh(%.5f) = %.5f" % (x,nn_tanh(x))
+def logloss(y, p):
+    J = 0.0
+    for j in range(len(y)):
+        J += y[j]*np.log(p[j])
+    return -J
 
-t = np.arange(-5.0, 5.0, 0.01)
+x = 0.2
+print "sigmoid(%.4f) = %.4f" % (x,sigmoid(x))
+print "tanh(%.4f) = %.4f" % (x,nn_tanh(x))
 
-l1 = plt.plot(t, sigmoid(t), 'r-')
-l2 = plt.plot(t, nn_tanh(t), 'g-')
 
-plt.show()
+x = np.array([1, 0, 0])
+w = np.array([0.2, -0.03, 0.14])
+z = w_sums(x, w)
+print "Returned:\n", z
+
+# W = np.array([[0.2, 0.15, -0.01],
+#               [0.01, -0.1, -0.06],
+#               [0.14, -0.2, -0.03]])
+# print W
+# print w_sums(x, W)
+
+# t = np.arange(-5.0, 5.0, 0.01)
+# l1 = plt.plot(t, sigmoid(t), 'r-')
+# l2 = plt.plot(t, nn_tanh(t), 'g-')
+# plt.show()

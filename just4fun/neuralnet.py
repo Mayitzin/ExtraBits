600302a296e2931f47ef00b8a2dd6d068ecccd2e
Mario Garcia <mayitzin@gmail.com>
2016-06-23 01:38:06 +0200

    Add softmax, logloss and computation of hidden layers

--- just4fun/neuralnet.py
+++ just4fun/neuralnet.py
@@ -8,21 +8,10 @@ http://www.existor.com/en/ml-neural-networks.html
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 def w_sums(x, w):
-    if len(np.shape(w))<2:
-        z = np.array([[0.0]])
-        w = np.array(w).reshape((len(w),1))
-    else:
-        z = np.zeros((np.shape(w)[0],1))
-    print np.shape(z)
-    print np.shape(w)
-    for i in range(np.shape(w)[0]):
-        for j in range(np.shape(z)[1]):
-            print "w[%d,%d]=%f"%(i,j,w[i,j])
-            # z[i] += x[i]*w[i,j]
-    return z
+    return np.dot(x,w)
 
 def sigmoid(x):
     return 1./(1+np.exp(-x))
@@ -30,11 +19,12 @@ def sigmoid(x):
 def nn_tanh(x):
     return np.tanh(x)
 
-def logloss(y, p):
-    J = 0.0
-    for j in range(len(y)):
-        J += y[j]*np.log(p[j])
-    return -J
+def softmax(z):
+    return np.exp(z)/np.sum(np.exp(z))
+
+def logloss(p, y):
+    return -np.dot(y,np.log(p))
+
 
 x = 0.2
 print "sigmoid(%.4f) = %.4f" % (x,sigmoid(x))
@@ -44,13 +34,25 @@ print "tanh(%.4f) = %.4f" % (x,nn_tanh(x))
 x = np.array([1, 0, 0])
 w = np.array([0.2, -0.03, 0.14])
 z = w_sums(x, w)
-print "Returned:\n", z
 
-# W = np.array([[0.2, 0.15, -0.01],
-#               [0.01, -0.1, -0.06],
-#               [0.14, -0.2, -0.03]])
-# print W
-# print w_sums(x, W)
+W = np.array([[[0.2, 0.15, -0.01],
+               [0.01, -0.1, -0.06],
+               [0.14, -0.2, -0.03]],
+              [[0.08, 0.11, -0.3],
+               [0.1, -0.15, 0.08],
+               [0.1, 0.1, -0.07]]])
+z1 = w_sums(x, W[0,:,:])
+h1 = nn_tanh(z1)
+print h1
+
+z2 = w_sums(h1, W[1,:,:])
+h2 = nn_tanh(z2)
+p = softmax(h2)
+print h2
+print p
+
+y = np.array([0, 1, 0])
+print logloss(p,y)
 
 # t = np.arange(-5.0, 5.0, 0.01)
 # l1 = plt.plot(t, sigmoid(t), 'r-')

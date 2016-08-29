8e7059795dd6a8125315441715cfc7314a8b5753
Mario Garcia <mayitzin@gmail.com>
2016-08-29 18:04:46 +0200

    Add details about date and time

--- ChronoTracker/chronos.py
+++ ChronoTracker/chronos.py
@@ -1,21 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Json handler for Chronological Tracker of entries.
+Handler for Chronological Tracker of entries.
 
 @author: Mario Garcia
 www.mayitzin.com
 """
 
 import json
+import datetime
 import time
-
-class Date(object):
-    """This class represents the dates"""
-    def __init__(self, date=time.strftime("%Y%m%d", time.gmtime())):
-        self.date = date
-
-    def isValid(self):
-        print self
 
 
 class Event(object):
@@ -28,7 +21,7 @@ class Event(object):
         self.month = time.strftime("%m", timeNow)
         self.day = time.strftime("%d", timeNow)
         self.date = self.day+"."+self.month+"."+self.year
-        # self.date = setDate(self)
+        self.setDate(self.date)
         # Set Information
         self.description = "Description of the event"
         self.references = ["This is a list of references"]
@@ -45,35 +38,51 @@ class Event(object):
         if "." in date:
             dateElems = date.split(".")
             if len(dateElems)==3:
-                self.year = self.dateID[:4]
-                self.month = self.dateID[4:6]
-                self.day = self.dateID[6:8]
+                self.month = dateElems[1]
+                if len(dateElems[0])==4:
+                    self.year = dateElems[0]
+                    self.day  = dateElems[2]
+                elif len(dateElems[2])==4:
+                    self.year = dateElems[2]
+                    self.day  = dateElems[0]
             self.dateID = "".join([self.year,self.month,self.day])
         if len(self.dateID)==8:
-            self.year = self.dateID[:4]
+            self.year  = self.dateID[:4]
             self.month = self.dateID[4:6]
-            self.day = self.dateID[6:8]
+            self.day   = self.dateID[6:8]
         self.date = self.day+"."+self.month+"."+self.year
-        self.dateID = self.year+self.month+self.day
+        self.dateID = str(date2unix(self.date))
+        print self.dateID
 
     def setDescription(self, description=None):
         self.description = description
         if self.description == None:
             self.description = ""
+        self.updateDict()
 
     def setReferences(self, references=None):
         self.references = references
         if self.references == None:
             self.references = [""]
+        self.updateDict()
 
     def setLinks(self, links=None):
         self.links = links
-        if self.links == None:
+        if self.links == None or len(self.links)==0:
             self.links = [""]
+        self.updateDict()
 
     def buildDict(self):
         self.d = {self.dateID:{self.date:{"year":self.year,"month":self.month,"day":self.day},"description":self.description,"references":self.references,"links":self.links}}
         return self.d
+
+    def updateDict(self):
+        self.d[self.dateID][self.date]["day"] = self.day
+        self.d[self.dateID][self.date]["month"] = self.month
+        self.d[self.dateID][self.date]["year"] = self.year
+        self.d[self.dateID]["description"] = self.description
+        self.d[self.dateID]["links"] = self.links
+        self.d[self.dateID]["references"] = self.references
 
     def saveEvent(self, fileName="output.dat"):
         with open(fileName, 'w') as jsonFile:
@@ -82,6 +91,41 @@ class Event(object):
     def printDict(self):
         print json.dumps(self.d, sort_keys=True, indent=4)
 
+
+def date2unix(dateAndTime):
+    year, day, month = 0, 0, 0
+    hour, mins, secs = 0, 0, 0
+    dateStr = str(day)+"."+str(month)+"."+str(year)
+    timeStr = str(hour)+":"+str(mins)+":"+str(secs)
+    if type(dateAndTime) != str:
+        print dateAndTime, "is not a string"
+        return dateAndTime
+    if " " in dateAndTime:
+        dateStr = dateAndTime.split(" ")[0]
+        timeStr = dateAndTime.split(" ")[1]
+    else:
+        dateStr = dateAndTime
+    if "." in dateStr:
+        dateElems = dateStr.split(".")
+        if len(dateElems)==3:
+            month = int(dateElems[1])
+            if len(dateElems[0])==4:
+                year = int(dateElems[0])
+                day  = int(dateElems[2])
+            elif len(dateElems[2])==4:
+                year = int(dateElems[2])
+                day  = int(dateElems[0])
+    if ":" in timeStr:
+        timeElems = timeStr.split(":")
+        hour = int(timeElems[0])
+        mins = int(timeElems[1])
+        if len(timeElems)==3:
+            secs = int(timeElems[2])
+    unixTime = datetime.datetime(year, month, day, hour, mins, secs)
+    return time.mktime(unixTime.timetuple())
+
+def unix2date(unixTime):
+    return datetime.datetime.fromtimestamp(float(unixTime)).strftime("%d.%m.%Y %H:%M:%S")
 
 def printInfo(event=None):
     if event == None:
@@ -95,19 +139,15 @@ def printInfo(event=None):
     print "\tLinks:", event.links
 
 
-new_event2 = Event()
-new_event2.setDate("1987.02.13")
-new_event2.setDescription("My Birthday")
-new_event2.setReferences(["ref1", "ref2"])
-new_event2.setLinks(["link1","link2"])
+new_event = Event()
+new_event.setDate("13.02.1987")
+new_event.setDescription("My Birthday")
+new_event.setReferences(["ref1", "ref2"])
+new_event.setLinks(["link1","link2"])
+new_event.saveEvent("output.txt")
 
-print new_event2.date
-print new_event2.description
+print new_event.date
+print new_event.description
 
-new_event2.printDict()
-
-printInfo(new_event2)
-
-printInfo()
-
-new_event2.saveEvent("output.txt")
+new_event.printDict()
+printInfo(new_event)

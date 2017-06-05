ed42b3e44af808c3098bd09baa30056d71f37681
unknown <mayitzin@gmail.com>
2017-06-05 16:25:30 +0200

    Add string to dict parser

--- VocabTrain/lexicon.py
+++ VocabTrain/lexicon.py
@@ -8,6 +8,7 @@ TODO: Make a good interpretation of stored data. Maybe parse as dictionary.
 import json
 from datetime import datetime
 
+
 def saveData(data, fileName="output.txt"):
     # Save to a file
     with open(fileName, 'a') as outfile:
@@ -16,22 +17,24 @@ def saveData(data, fileName="output.txt"):
 def readData(fileName, printOut=False):
     with open(fileName, 'r') as inFile:
         read_lines = inFile.readlines()
+    print("Read", len(read_lines), "lines")
     if len(read_lines)>0 and printOut:
         for line in read_lines:
-            print line.strip()
-            #print line.strip()[1:-1]
-            #print line.strip().split(',')
+            print(line.strip())
+        last_line = read_lines[-1]
+        new_d = json.loads(last_line)
+        print(json.dumps(new_d, sort_keys=True, indent=4))
+
+def newEntry():
+    t = datetime.now().isoformat()
+    d = {t: {'bar': ('baz', None, 1.0, 2)}}
+    return d
 
 dic_EN_GE = {"label1":"label"}
 
-t = datetime.now().isoformat()
-
-#s = [t, {'bar': ('baz', None, 1.0, 2)}]
-d = {t: {'bar': ('baz', None, 1.0, 2)}}
-
+d = newEntry()
 
 # Compact encoding
-#data_line = json.dumps(s, sort_keys=True, separators=(',',':')) + "\n"
 data_line = json.dumps(d, sort_keys=True, separators=(',',':')) + "\n"
 
 # Save to a file

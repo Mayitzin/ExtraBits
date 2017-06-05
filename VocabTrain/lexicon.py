d76802e34e346ad0ce94a09bfe8dbc0d25d0ffee
Mayitzin <mayitzin@gmail.com>
2017-06-05 11:40:01 +0200

    Add datetime in text. For testing

--- VocabTrain/lexicon.py
+++ VocabTrain/lexicon.py
@@ -1,13 +1,39 @@
 """
 This script creates, modifies and removes a quasi-database of a lexicon using
 JSON formatted data.
+
+TODO: Make a good interpretation of stored data. Maybe parse as dictionary.
 """
 
 import json
+from datetime import datetime
+
+def saveData(data, fileName="output.txt"):
+    # Save to a file
+    with open(fileName, 'a') as outfile:
+        outfile.write(data)
+
+def readData(fileName, printOut=False):
+    with open(fileName, 'r') as inFile:
+        read_lines = inFile.readlines()
+    if len(read_lines)>0 and printOut:
+        for line in read_lines:
+            print line.strip()
+            #print line.strip()[1:-1]
+            #print line.strip().split(',')
 
 dic_EN_GE = {"label1":"label"}
 
-# with open('data.txt', 'w') as outfile:
-#     json.dump(data, outfile)
+t = datetime.now().isoformat()
 
-print json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}], sort_keys=True, indent=4)
+#s = [t, {'bar': ('baz', None, 1.0, 2)}]
+d = {t: {'bar': ('baz', None, 1.0, 2)}}
+
+
+# Compact encoding
+#data_line = json.dumps(s, sort_keys=True, separators=(',',':')) + "\n"
+data_line = json.dumps(d, sort_keys=True, separators=(',',':')) + "\n"
+
+# Save to a file
+saveData(data_line, "output.txt")
+readData("output.txt", printOut=True)
